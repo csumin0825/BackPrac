3c0fcc8f5d120835cb9ce0e436428bff2c1831e8
Sumin <77442721+csumin0825@users.noreply.github.com>
2022-04-05 23:58:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 // // let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-// let input = fs.readFileSync("input.txt").toString().split("\n");
 
 // for (i = 0; i < input.length; i++) {
 //   const n = input[i].split(" ");
