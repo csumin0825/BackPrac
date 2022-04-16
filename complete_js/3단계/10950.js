995fd87a62d4cbae776cb5684d7d2c65742abaa1
Sumin <77442721+csumin0825@users.noreply.github.com>
2022-04-16 23:43:23 +0900

    Update 10950.js

--- complete_js/3단계/10950.js
+++ complete_js/3단계/10950.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 
 let input = fs.readFileSync("input.txt").toString().split("\n");
-// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
 let t = parseInt(input[0]);
 console.log(typeof t);
 
