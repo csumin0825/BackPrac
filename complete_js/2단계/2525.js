cbda7e588517eab590c07b4f65d42ef2cb51a2d6
Sumin <77442721+csumin0825@users.noreply.github.com>
2022-04-28 23:51:12 +0900

    Update 2525.js

--- complete_js/2단계/2525.js
+++ complete_js/2단계/2525.js
@@ -2,11 +2,6 @@ const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 
 // let input = fs.readFileSync("input.txt").toString().split(" ");
-// let input = fs.readFileSync("input.txt").toString().split("\n");
-
-// let input = fs.readFileSync("/dev/stdin").toString().split(" ");
-// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 const time = input[0].split(" ");
 
