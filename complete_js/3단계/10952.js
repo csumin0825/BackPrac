ab69f98deec8616446c5a585dbc499945171c30d
Sumin <77442721+csumin0825@users.noreply.github.com>
2022-05-19 23:57:11 +0900

    Update 10952.js

--- complete_js/3단계/10952.js
+++ complete_js/3단계/10952.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-// let input = fs.readFileSync("input.txt").toString().split("\n");
 let i = 0;
 
 while (true) {
