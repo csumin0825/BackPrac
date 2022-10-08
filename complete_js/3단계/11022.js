f61ef1f00ddc295c78cd7b752341ef7429ef2509
Sumin <77442721+csumin0825@users.noreply.github.com>
2022-10-08 23:21:43 +0900

    Update 11022.js

--- complete_js/3단계/11022.js
+++ complete_js/3단계/11022.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n"); //
+let input = fs.readFileSync("/dev/stdin").toString().split("\n"); 
 
 let ans = "";
 
