a5a148e38b71bc230ab2469cd20e59d45c315651
Sumin <77442721+csumin0825@users.noreply.github.com>
2022-12-14 23:59:32 +0900

    Update 2753.js

--- complete_js/2단계/2753.js
+++ complete_js/2단계/2753.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split(" ");
-//let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 
 
 const a = parseInt(input[0]);
