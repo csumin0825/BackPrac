625591a4ba083de0fdd4e28668ee95b6d397c7f8
sumin <csumin008255@gmail.com>
2023-03-03 18:16:38 +0900

    23.03.03_9020.js

--- complete_js/9020.js
+++ complete_js/9020.js
@@ -2,19 +2,86 @@
 // const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const fs = require("fs");
-let input = fs.readFileSync("example.txt").toString().split("\n");
+// const fs = require("fs");
+// let input = fs.readFileSync("example.txt").toString().split("\n");
 
-let n = Number(input.shift());
-input = input.map(Number);
+// let n = Number(input.shift());
+// input = input.map(Number);
+// let max = Math.max(...input);
+// let arr = new Array(max + 1).fill(true);
 
-let arr = new Array(n + 1).fill(true);
+// for (let i = 0; i < Math.sqrt(max); i++) {
+//   if (i === 0 || i === 1) arr[i] = false;
+//   if (!arr[i]) continue;
+//   for (let j = 2; j * i < max; j++) {
+//     arr[i * j] = false;
+//   }
+// }
 
-for (let i = 2; i < Math.sqrt(n); i++) {
-  if (!arr[i]) continue;
-  for (let j = 2; j * i < n; j++) {
-    arr[i * j] = false;
+// input.forEach((el) => {
+//   console.log(gold(el).join(" "));
+// });
+
+// function gold(num) {
+//   let result = [];
+
+//   for (let i = 2; i < num; i++) {
+//     if (arr[i]) {
+//       if (arr[num - i]) {
+//         result.push([i, num - i]);
+//       }
+//     }
+//   }
+//   result.sort((a, b) => Math.abs(a[0] - a[1]) - Math.abs(b[0] - b[1]));
+//   return result[0];
+// }
+
+/** require 사용 시 typeError
+ *  readline 사용함
+ */
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  let n = Number(input.shift());
+  input = input.map(Number);
+  let max = Math.max(...input);
+  let arr = new Array(max + 1).fill(true);
+
+  for (let i = 0; i < Math.sqrt(max); i++) {
+    if (i === 0 || i === 1) arr[i] = false;
+    if (!arr[i]) continue;
+    for (let j = 2; j * i < max; j++) {
+      arr[i * j] = false;
+    }
+  }
+
+  input.forEach((el) => {
+    console.log(gold(el).join(" "));
+  });
+
+  function gold(num) {
+    let result = [];
+
+    for (let i = 2; i < num; i++) {
+      if (arr[i]) {
+        if (arr[num - i]) {
+          result.push([i, num - i]);
+        }
+      }
+    }
+    result.sort((a, b) => Math.abs(a[0] - a[1]) - Math.abs(b[0] - b[1]));
+    return result[0];
   }
 }
-
-console.log(arr);
