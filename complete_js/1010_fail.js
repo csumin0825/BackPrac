2f86c164756b87fe0928aaba83de67a9099ebcd4
sumin <csumin008255@gmail.com>
2023-03-03 21:57:09 +0900

    23.03.03_1010_fail.js

--- complete_js/1010_fail.js
+++ complete_js/1010_fail.js
@@ -8,16 +8,16 @@ let input = fs.readFileSync("example.txt").toString().split("\n");
 const t = Number(input.shift());
 input = input.map((x) => x.split(" ").map(Number));
 
-let arr = new Array(30).fill(0);
-for (let i = 0; i < 30; i++) {
-  let miniArr = new Array(30).fill(0);
+let arr = new Array(30 + 1).fill(0);
+for (let i = 0; i < 30 + 1; i++) {
+  let miniArr = new Array(30 + 1).fill(0);
   arr[i] = miniArr;
 }
 
-for (let i = 1; i < 30; i++) {
-  for (let j = 1; j < 30; j++) {
-    if (i === j) arr[i][j] = 1;
-    else if (i === 1) arr[i][j] = j;
+for (let i = 1; i <= 30; i++) {
+  for (let j = i; j <= 30; j++) {
+    if (i === 1) arr[i][j] = j;
+    else if (i === j) arr[i][j] = 1;
     else if (i < j) {
       arr[i][j] = arr[i - 1][j - 1] + arr[i][j - 1];
     }
@@ -26,5 +26,6 @@ for (let i = 1; i < 30; i++) {
 
 input.forEach((el) => {
   const [n, m] = el;
+
   console.log(arr[n][m]);
 });
