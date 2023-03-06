dceb8c1f93e62e4b20fcb850ef5de139f6293d41
sumin <csumin008255@gmail.com>
2023-03-07 00:08:07 +0900

    23.03.07_1181.js

--- complete_js/1181.js
+++ complete_js/1181.js
@@ -3,18 +3,10 @@
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const fs = require("fs");
-let input = fs.readFileSync("example.txt").toString().split("\n");
+let input = fs.readFileSync("example.txt").toString().trim().split("\n");
 
 const n = Number(input.shift());
 let set = new Set(input);
 const words = [...set];
 
-words.sort(function (a, b) {
-  if (a.length === b.length) {
-    return a.localeCompare(b);
-  } else {
-    return a.length - b.length;
-  }
-});
-
-console.log(words.join("\n"));
+console.log(words.sort((a, b) => a.length - b.length || a.localeCompare(b)).join("\n"));
